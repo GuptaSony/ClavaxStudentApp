dd6c238c2ad7a5b15d4c25595259f092784574a8
sony gupta <sonyg4517@gmail.com>
2021-01-28 15:33:59 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentDashboardComponent
   ],
   imports: [
     BrowserModule,
